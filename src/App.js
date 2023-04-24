d26178ea562d42afb67f56771907d9076bcf5202
Ethan <ethan.nordahl1@gmail.com>
2023-04-23 23:51:33 -0700

    half done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Featured from './components/Featured';
+import Highlights from './components/Highlights';
 import Landing from './components/Landing';
 import Nav from './components/Nav'
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -8,6 +13,11 @@ function App() {
       <div className="App">
         <Nav />
         <Landing />
+        <Highlights />
+        <Featured />
+        <Discounted />
+        <Explore />
+        <Footer />
       </div>
   );
 }
